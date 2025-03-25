1a651e28a7e180bd892fb957d9b5e1a8b4706d94
Igor Stefaniak <igorstefaniak07@gmail.com>
2025-03-25 15:52:37 +0100

    pobieranie ścieżki i wyświetlanie jej

--- FileManager.java
+++ FileManager.java
@@ -1,4 +1,6 @@
+
 import java.io.File;
+import java.util.Scanner;
 
 public class FileManager {
 
@@ -8,7 +10,26 @@ public class FileManager {
         currentDirectory = new File(System.getProperty("user.dir")); //skąd aplikacja jest uruchomiona
     }
 
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        System.out.println("Nawigator plików: użyj /? aby wyświetlić pomoc.");
+        while (true) {
+            System.out.print(currentDirectory.getAbsolutePath() + " > ");
+            command = scanner.nextLine().trim();
+            if (command.equalsIgnoreCase("q")) {
+                break;
+            }
+            processCommand(command);
+        }
+    }
+
+    private void processCommand(String command) {
+        //TODO przetwarzanie komend
+    }
+
     public static void main(String[] args) {
-        
+        FileManager manager = new FileManager();
+        manager.run();
     }
 }
