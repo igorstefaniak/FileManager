6c93bbed097d97167639da690ccd99a3d4f4615c
Igor Stefaniak <igorstefaniak07@gmail.com>
2025-04-02 22:45:38 +0200

    przetwarzanie komend

--- CommandHandler.java
+++ CommandHandler.java
@@ -1,8 +1,9 @@
 package filemanager;
+
 import java.util.Scanner;
 
 public class CommandHandler {
-    
+
     private FileOperations fileOperations;
 
     public CommandHandler(FileOperations fileOperations) {
@@ -12,10 +13,10 @@ public class CommandHandler {
     public void processCommands() {
         try (Scanner scanner = new Scanner(System.in)) {
             String command;
-            
+
             System.out.println("Nawigator plików: użyj /? lub /h aby wyświetlić pomoc.");
             while (true) {
-                System.out.print("PLR " + fileOperations.getCurrentDirectory().getAbsolutePath() + "> " ); //➜
+                System.out.print("PLR " + fileOperations.getCurrentDirectory().getAbsolutePath() + "> "); //➜
                 command = scanner.nextLine().trim();
                 if (command.equalsIgnoreCase("/q") || command.equalsIgnoreCase("/exit") || command.equalsIgnoreCase("/quit")) {
                     break;
@@ -24,24 +25,55 @@ public class CommandHandler {
             }
         }
     }
+
     private void processCommand(String command) {
-        //TODO przetwarzanie komend
         String[] tokens = command.split("\\s+");
         if (tokens.length == 0) {
             return;
         }
         switch (tokens[0].toLowerCase()) {
             case "/?", "/help", "/h" -> printHelp();
+            case "/ls", "/dir" -> fileOperations.listFiles();
+            case "/cd" -> {
+                if (tokens.length < 2) {
+                    System.out.println("Użycie: cd <ścieżka>");
+                } else {
+                    fileOperations.changeDirectory(tokens[1]);
+                }
+            }
+            case "/mkdir" -> {
+                if (tokens.length < 2) {
+                    System.out.println("Użycie: mkdir <nazwa_folderu>");
+                } else {
+                    fileOperations.createDirectory(tokens[1]);
+                }
+            }
+            case "/touch", "/mkfile" -> {
+                if (tokens.length < 2) {
+                    System.out.println("Użycie: touch <nazwa_pliku>");
+                } else {
+                    fileOperations.createFile(tokens[1]);
+                }
+            }
+            case "/rm" -> {
+                if (tokens.length < 2) {
+                    System.out.println("Użycie: rm <nazwa_pliku/folderu>");
+                } else {
+                    fileOperations.delete(tokens[1]);
+                }
+            }
+            default -> System.out.println("Nieznana komenda. Wpisz '/h' aby uzyskać pomoc.");
         }
     }
+
     private void printHelp() {
         //TODO sformatować do tabelki
         System.out.println("Dostępne komendy:");
         System.out.println("/help, /?, /h- wyświetla pomoc");
-        System.out.println("/ls- listuje pliki i foldery");
+        System.out.println("/ls, /dir- listuje pliki i foldery");
         System.out.println("/cd <ścieżka>- zmienia katalog (użyj '..' aby wrócić do katalogu nadrzędnego)");
         System.out.println("/mkdir <nazwa>- tworzy folder");
-        System.out.println("/mkfile <nazwa>- tworzy pusty plik");
+        System.out.println("/mkfile, /touch <nazwa>- tworzy pusty plik");
         System.out.println("/rm <nazwa>- usuwa plik lub folder (foldery usuwane rekurencyjnie)");
         System.out.println("/exit, /quit, /q- wychodzi z programu");
     }
--- FileOperations.java
+++ FileOperations.java
@@ -12,5 +12,30 @@ public class FileOperations {
     public File getCurrentDirectory() {
         return currentDirectory;
     }
+
+    public Object listFiles() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listFiles'");
+    }
+
+    public void changeDirectory(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changeDirectory'");
+    }
+
+    public void createDirectory(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createDirectory'");
+    }
+
+    public void createFile(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createFile'");
+    }
+
+    public void delete(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
     
 }
